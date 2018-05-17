e460048b8d4ae6485f84ece6e04d4253e6bc7040
Jarvis <wangjia_1919@163.com>
2018-05-17 17:14:51 +0800

    修改高可用

--- src/main/java/com/springcloudify/cloudy/configclient/ConfigClientApplication.java
+++ src/main/java/com/springcloudify/cloudy/configclient/ConfigClientApplication.java
@@ -1,9 +1,11 @@
 package com.springcloudify.cloudy.configclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringCloudApplication
+//@SpringCloudApplication
+@SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
